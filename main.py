585b429857d61a03145e019d039b2f3b799c48af
Mahdoosh1 <mahdiyadiilavi1@gmail.com>
2024-11-16 21:42:38 +0330

    Update

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from math import log2, ceil
+from types import NoneType
+
+
 def size_of(value):
     if not isinstance(value, int):
         return len(value)
@@ -7,7 +10,7 @@ def size_of(value):
     else:
         a = ceil(log2(value+1))
     return max(ceil(a/8),1)
-class Structure:
+class DeprecatedStructure:
     def __init__(self):
         self.value = b""
         self.dict = {}
@@ -16,6 +19,9 @@ class Structure:
         self.struct = {}
     def assign_var(self, name, value):
         self.vars[name] = value
+    def assign_vars(self, pairs):
+        for name, value in pairs:
+            self.assign_var(name, value)
     def assign_calculation(self, names, function, function_name, out_name=None):
         if out_name is None:
             out_name = function_name
@@ -71,41 +77,83 @@ class Structure:
         for key in self.dict.keys():
             out[key] = self.get_value(key)
         return out
-def double_size(data):
-    a = size_of(len(data))
-    b = size_of(a)
-    c = b.to_bytes(2,'big')+a.to_bytes(b,'big')
-    return c
-def file(data, name):
-    if "/" in name:
-        raise NameError("Name should not contain slashes")
-    if len(name) > 254:
-        raise NameError("Name should not be more than 254 charactors")
-    if len(data) > 1024**3:
-        raise ValueError("Data should not be more than 1GB")
-    s = Structure()
-    s.assign_var("name",name.encode('utf-8'))
-    s.assign_var("data",data)
-    s.assign_var("type",b"\x00")
-    s.assign_var("name_size",len(name))
-    s.assign_var("data_size",double_size(data))
-    s.struct_values(["type","name_size","name","data_size","data"])
-    return s.value
-def directory(files,name):
-    if "/" in name:
-        raise NameError("Name should not contain slashes")
-    if len(name) > 254:
-        raise NameError("Name should not be more than 254 charactors")
-    if size_of(len(files)) > 10**5:
-        raise ValueError("There should not be more than 10,000 files")
-    s = Structure()
-    s.assign_var("name",name.encode('utf-8'))
-    s.assign_var("file_len",len(files).to_bytes(2,'big'))
-    s.assign_var("files",b"".join(files))
-    s.assign_var("type",b"\x01")
-    s.assign_var('name_size',len(name))
-    s.struct_values(["type","name_size","name","file_len","files"])
-    return s.value
+class Structure:
+    def __init__(self):
+        self.funcs = {}
+        self.struct = {} #{name, required=True}
+        self.frozen = False
+    def freeze(self):
+        self.frozen = True
+    def update_struct(self, pairs):
+        if self.frozen:
+            raise AttributeError("This object is frozen")
+        for i in pairs:
+            if isinstance(i, tuple):
+                self.struct[i[0]] = i[1]
+            else:
+                self.struct[i] = True
+    def add_func(self, input_names, func, output_name, name=None):
+        if self.frozen:
+            raise AttributeError("This object is frozen")
+        if name is None:
+            name = output_name
+        self.funcs.update({name: [func, input_names, output_name]})
+    def run_funcs(self, variables, *, log=True):
+        for name, (func, input_names, output_name) in self.funcs.items():
+            inputs = [variables[i] for i in input_names]
+            output = func(*inputs)
+            if not output is None:
+                variables[output_name] = output
+            if log:
+                print(f"Function {name} success")
+        struct = []
+        for name, req in self.struct.items():
+            if name in variables.keys():
+                if not any([isinstance(variables[name], bytes),
+                            isinstance(variables[name], bytearray),
+                            isinstance(variables[name], NoneType),
+                            isinstance(variables[name], str)]):
+                    raise TypeError(f"Variable {name} Should return bytes or bytearray object")
+                if isinstance(variables[name], str):
+                    struct.append(variables[name].encode('utf-8'))
+                else:
+                    struct.append(variables[name])
+            elif req:
+                raise EOFError("Required Structure Variable {name} missing")
+        return b"".join(struct)
+
+
+FILE = Structure()
+FILE.update_struct(["type","name_len","name","len_len","data_len","data"])
+FILE_NAME_LEN = lambda x: len(x).to_bytes(1,'big')
+FILE_LEN_LEN = lambda x: x.to_bytes(1,'big')
+FILE_DATA_LEN_INT = lambda x: len(x)
+FILE_DATA_LEN = lambda x, y: x.to_bytes(y,'big')
+FILE_TYPE = lambda : b'\x00'
+FILE.add_func([],FILE_TYPE,"type")
+FILE.add_func(["name"],FILE_NAME_LEN,"name_len")
+FILE.add_func(["data"],FILE_DATA_LEN_INT,"data_len_int")
+FILE.add_func(["data_len_int"],size_of,"len_len_int")
+FILE.add_func(["len_len_int"],FILE_LEN_LEN,"len_len")
+FILE.add_func(["data_len_int","len_len_int"],FILE_DATA_LEN,"data_len")
+FILE.freeze()
+
+file = lambda vars: FILE.run_funcs(variables=vars)
+
+DIR = Structure()
+DIR.update_struct(["type","name_len","name","file_count","file_bytes"])
+DIR_NAME_LEN = lambda x: len(x).to_bytes(1,'big')
+DIR_TYPE = lambda : b'\x01'
+DIR_JOIN_FILES = lambda x: b''.join(x)
+DIR_COUNT_FILES = lambda x: len(x).to_bytes(2,'big')
+DIR.add_func([],DIR_TYPE,"type")
+DIR.add_func(["name"],DIR_NAME_LEN,"name_len")
+DIR.add_func(["files"],DIR_JOIN_FILES,"file_bytes")
+DIR.add_func(["files"],DIR_COUNT_FILES,"file_count")
+DIR.freeze()
+
+dir = lambda vars: DIR.run_funcs(variables=vars)
+
 def append_to_nested_list(main_list, target_list, data):
     def recursive_search(sub_list, target_index):
         if target_index == len(target_list):
@@ -124,19 +172,19 @@ def append_to_nested_list(main_list, target_list, data):
             if recursive_search(item[1], 1):
                 return main_list
     return main_list
-def to_files(root):
+def to_files(root, _archive=True):
     out = []
     for type,name,data in root:
         if type:
-            out.append(directory(to_files(data),name))
+            out.append(dir({
+                "name":name,
+                "files":to_files(data,False)
+            }))
         else:
-            out.append(file(data,name))
+            out.append(file({
+                "name":name,
+                "data":data
+            }))
+    if _archive:
+        return b''.join(out)
     return out
-def archive(root):
-    return directory(to_files(root),'~')
-root = [(0,"meow.txt",b"meow"),(1,"meow",[(0,"meow2.txt",b"meow2")])]
-#file format: type(0), name, data
-#dir format: type(1), name, list of files or dirs
-print(archive(root)) #bytes object
-#output file format: type(\x00), name_len(1byte),name,len_len(2bytes),file_len(1-256 bytes),file
-#output dir format: type(\x01),name_len(1byte),name,number_of_things_inside(2bytes),things_inside
